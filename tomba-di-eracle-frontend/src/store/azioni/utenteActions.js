fe8cc49271d46a602df5c8d2980f7ebafe33035a
Paolo Marchitto <paolo.marchitto88@gmail.com>
2021-03-24 12:38:32 +0100

    Ultimato componente Login con controlli campi vuoti e credenziali

--- tomba-di-eracle-frontend/src/store/azioni/utenteActions.js
+++ tomba-di-eracle-frontend/src/store/azioni/utenteActions.js
@@ -3,12 +3,13 @@ import UtenteService from "../../servizi/UtenteService"
 export const login = (utente) => {
     return (dispatch) => {
         UtenteService.login(utente).then(res => {
+            sessionStorage.setItem('utente', JSON.stringify(res.data))
             dispatch({
                 type: 'LOGIN_UTENTE',
                 utente: res.data
             })
         }).catch(err => {
-            
+            alert('Credenziali errate')
         })
     }
 }
