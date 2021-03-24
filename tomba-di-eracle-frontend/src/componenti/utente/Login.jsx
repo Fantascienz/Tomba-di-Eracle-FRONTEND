eb395aba97c8b1b169058e760f8d9303568fe575
Paolo Marchitto <paolo.marchitto88@gmail.com>
2021-03-24 12:11:38 +0100

    implementazione Service e Reducer dell'entità Utente

--- tomba-di-eracle-frontend/src/componenti/utente/Login.jsx
+++ tomba-di-eracle-frontend/src/componenti/utente/Login.jsx
@@ -1,23 +1,39 @@
+import React, { Component } from "react";
+
 class Login extends Component {
-    
+
     state = {
         email: '',
         psw: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+
     }
 
     render() {
         return (
             <div>
                 <div className="row">
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className="input-group mb-3">
                             <span className="input-group-text">Email</span>
                             <input className="form-control" type="email" id="email" value={this.state.email} onChange={this.handleChange} />
                         </div>
 
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">Password</span>
+                            <input className="form-control" type="password" id="psw" value={this.state.psw} onChange={this.handleChange} />
+                        </div>
+                        <button className="btn btn-primary">Entra</button>
                     </form>
                 </div>
             </div>
         );
     }
 }
+
+export default Login;
--- tomba-di-eracle-frontend/src/store/reducers/rootReducer.js
+++ tomba-di-eracle-frontend/src/store/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux';
+import utenteReducer from './utenteReducer';
 
 const rootReducer = combineReducers({
-
+    utente: utenteReducer
 })
 
 export default rootReducer;
