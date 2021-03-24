01365dc766db2cfbcce6215e85f830c074d4ebc4
Tiziano Massa <tiziano.max@gmail.com>
2021-03-24 12:41:20 +0100

    form registrazione completato

--- tomba-di-eracle-frontend/src/App.js
+++ tomba-di-eracle-frontend/src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Footer from './componenti/layout/Footer';
 import Header from './componenti/layout/Header';
+<<<<<<< HEAD
 import Login from './componenti/utente/Login';
 
+=======
+import Registrazione from './componenti/Registrazione';
+>>>>>>> 0efbf89 (form registrazione completato)
 
 function App() {
   return (
@@ -13,7 +17,12 @@ function App() {
 
       <Router>
         <Switch>
+<<<<<<< HEAD
           <Route path="/" exact component={Login}></Route>
+=======
+          <Route path="/" exact component={""}></Route>
+          <Route path="/registrazione" component={Registrazione}/>
+>>>>>>> 0efbf89 (form registrazione completato)
         </Switch>
       </Router>
 
--- tomba-di-eracle-frontend/src/componenti/Registrazione.jsx
+++ tomba-di-eracle-frontend/src/componenti/Registrazione.jsx
@@ -3,20 +3,59 @@ import React, { Component } from 'react';
 class Registrazione extends Component {
 
     state = {
+        nome: '',
+        cognome: '',
+        email: '',
+        psw: '',
+        psw2: ''
+    }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
     }
 
     render() {
         return (
-            <React.Fragment>
-                <div className="form-row">
-                    <div className="form-group col-md-6">
-                        <label htmlFor="nome">Nome</label>
-                        <input type="text" id="nome" className="form-control" placeholder="Nome"/>
-                    </div>
+            <div style={{ position: "fixed", top: "10%", width: "100%", height: "85%" }}>
+                <div className="container" style={{ marginTop: "5%" }}>
+                    <h1>Registrazione</h1>
+                    <br />
+                    <div style={{ align: "center" }}>
+                        <form>
+                            <div className="row">
+                                <h3>Nominativo</h3>
+                                <div className="col-md-3"></div>
+                                <div className="form-group col-md-3">
+                                    <input type="text" id="nome" placeholder="Nome" className="form-control" onChange={this.handleChange} /> <br />
+                                </div>
+                                <div className="form-group col-md-3">
+                                    <input type="text" id="cognome" placeholder="Cognome" className="form-control" onChange={this.handleChange}/> <br />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <h3>Email</h3>
+                                <div className="col-md-3"></div>
+                                <div className="form-group col-md-6">
+                                    <input type="email" id="email" placeholder="Email" className="form-control" onChange={this.handleChange}/> <br />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <h3>Password</h3>
+                                <div className="col-md-3"></div>
+                                <div className="form-group col-md-3">
+                                    <input type="password" id="psw" placeholder="Password" className="form-control" onChange={this.handleChange} /> <br />
+                                </div>
+                                <div className="form-group col-md-3">
+                                    <input type="password" id="psw2" placeholder="Conferma Password" className="form-control" onChange={this.handleChange} /> <br />
+                                </div>
+                            </div>
+                            <button className="btn btn-primary"type="submit">Registrati</button>
+                        </form>
+                    </div >
                 </div>
-
-            </React.Fragment>
+            </div>
         );
     }
 }
