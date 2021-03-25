539300e007075c04a6efadd91b4588e607870530
Gianluca Spadazzi <g.spadazzijs@gmail.com>
2021-03-25 09:08:55 +0100

    risolto conflitto in Utente.jsx

--- tomba-di-eracle-frontend/src/componenti/utente/Utente.jsx
+++ tomba-di-eracle-frontend/src/componenti/utente/Utente.jsx
@@ -25,9 +25,9 @@ class Utente extends Component {
                     <h1>Salute {JSON.parse(sessionStorage.getItem('utente')).nominativo}</h1>
                     <div className="row">
                         <div className="col-md-4">
-                            <img src="https://cdn.discordapp.com/attachments/823502374106038273/823573400621678592/Simbolo_TombaDiEracle.jpg" className="tombaJPG rounded-circle" alt="" /> <br /><br />
-                            <button className="btn btn-dark" style={{ color: "#eeaa44" }} onClick={() => this.toCreazionePersonaggio()}>Crea Personaggio</button> <br /><br />
-                            <button className="btn btn-dark" style={{ color: "#eeaa44" }} onClick={() => this.props.toModificaUtente()}>Modifica Account</button>
+                            <img src="https://cdn.discordapp.com/attachments/823502374106038273/823573400621678592/Simbolo_TombaDiEracle.jpg" className="tombaJPG rounded-circle" alt="" style={{  boxShadow: "0 24px 32px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)" }}/> <br /><br />
+                            <button className="btn btn-dark" style={{ color: "#eeaa44", width:"200px" }} onClick={() => this.toCreazionePersonaggio()}>Crea Personaggio</button> <br /><br />
+                            <button className="btn btn-dark" style={{ color: "#eeaa44", width:"200px" }} onClick={() => this.props.toModificaUtente()}>Modifica Account</button>
                         </div>
                         <div className="col-md-8">
 
