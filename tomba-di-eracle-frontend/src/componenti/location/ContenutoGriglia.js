e70ac586b9a01544c5fae494bcaca4e74721356b
Gianluca Spadazzi <g.spadazzijs@gmail.com>
2021-04-01 15:28:14 +0200

    sistemata MACROMAPPA, creati componenti ed installato react-image-magnifiers

--- tomba-di-eracle-frontend/src/componenti/location/ContenutoGriglia.js
+++ tomba-di-eracle-frontend/src/componenti/location/ContenutoGriglia.js
@@ -1,10 +1,10 @@
 import star from '../../img/red_star.png'
 
 
-export const Puntatore = ({display, idCella}) => {
+export const Puntatore = ({ display, idCella }) => {
 
-    function attivaDisplay() { 
-        if (display == idCella){
+    function attivaDisplay() {
+        if (display == idCella) {
             return ('')
         } else {
             return ('none')
@@ -12,8 +12,36 @@ export const Puntatore = ({display, idCella}) => {
     };
 
     return (
-        <div className="centrato" style={{position:"relative", marginLeft:"-50%", backgroundColor:"transparent", height:"100%", width:"200%"}}>
-            <img src={star} alt="..." style={{ height:"100%", width:"auto", display:`${attivaDisplay()}` }}/>
+        <div className="centrato" style={{ position: "relative", marginLeft: "-50%", backgroundColor: "transparent", height: "100%", width: "200%" }}>
+            <img src={star} alt="..." style={{ height: "100%", width: "auto", display: `${attivaDisplay()}` }} />
         </div>
     )
 }
+
+
+
+export const RigaGriglia = ({inizio, fine, idLocation, pxDimensioniMappa}) => {
+
+    var pxDimensioniCella = (pxDimensioniMappa/12);
+
+    function cellaGriglia() {
+        let arrayGriglia = []
+        for (let i = inizio; i<=fine; i++) {
+                arrayGriglia.push({
+                    codice:
+                        <div className="col-sm-1 griglia-macromappa" title={i} id={i} style={{height: `${pxDimensioniCella}px`, width: `${pxDimensioniCella}px`}}>
+                            <Puntatore display={idLocation} idCella={i} />
+                        </div>
+                })
+        }
+        return arrayGriglia;
+    }
+
+    return (
+        <div className="row" style={{ marginLeft: "0.01%" }}>
+            {cellaGriglia().map(cella => 
+                cella.codice)}
+        </div>
+    )
+}
+
--- tomba-di-eracle-frontend/src/componenti/location/Macromappa.jsx
+++ tomba-di-eracle-frontend/src/componenti/location/Macromappa.jsx
@@ -1,244 +1,35 @@
-import React, { Component } from 'react';
 import macromappa from '../../img/macromappa.jpg'
-import { Puntatore } from '../location/ContenutoGriglia'
+import { RigaGriglia } from '../location/ContenutoGriglia'
+import { Magnifier } from 'react-image-magnifiers'
 
-class Macromappa extends Component {
 
-    idLocation = "1";
+const Macromappa = ({ idLocation, pxDimensioniMappa, lenteDisplay }) => {
 
-    render() {
-        return (
+    idLocation = "100";
+    pxDimensioniMappa = "500" //min 320
+    lenteDisplay = ""
+
+    return (
+        <>
             <div>
-                <div style={{ height: "600px", width: "600px", top: "0", left: "50px", backgroundImage: `url('${macromappa}')`, backgroundPosition: "0px 0px", backgroundSize: "contain", backgroundRepeat: "no-repeat" }}>
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="1" id="1">
-                            <Puntatore display={this.idLocation} idCella="1" />
-                        </div>
+                <div style={{ position: "relative", height: `${pxDimensioniMappa}px`, width: `${pxDimensioniMappa}px`, top: "0", left: "50px" }}>
 
-                        <div className="col-sm-1 griglia-macromappa" title="2" id="2">
-                            <Puntatore display={this.idLocation} idCella="2" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="3" id="3">
-                            <Puntatore display={this.idLocation} idCella="3" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="4" id="4">
-                            <Puntatore display={this.idLocation} idCella="4" />
-                        </div>
-                        
-                        <div className="col-sm-1 griglia-macromappa" title="5" id="5">
-                            <Puntatore display={this.idLocation} idCella="5" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="6" id="6">
-                            <Puntatore display={this.idLocation} idCella="6" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="7" id="7">
-                            <Puntatore display={this.idLocation} idCella="7" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="8" id="8">
-                            <Puntatore display={this.idLocation} idCella="8" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="9" id="9">
-                            <Puntatore display={this.idLocation} idCella="9" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="10" id="10">
-                            <Puntatore display={this.idLocation} idCella="10" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="11" id="11">
-                            <Puntatore display={this.idLocation} idCella="11" />
-                        </div>
-
-                        <div className="col-sm-1 griglia-macromappa" title="12" id="12">
-                            <Puntatore display={this.idLocation} idCella="12" />
-                        </div>
+                    <div style={{ position: "absolute", zIndex: "1", height: "100%", width: "100%" }}>
+                        <img id="giovanni" src={macromappa} alt="..." style={{ height: "100%", width: "100%" }} />
                     </div>
 
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="13" id="13"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="14" id="14"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="15" id="15"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="16" id="16"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="17" id="17"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="18" id="18"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="19" id="19"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="20" id="20"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="21" id="21"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="22" id="22"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="23" id="23"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="24" id="24"></div>
+                    <div style={{ position: "relative", zIndex: "2", height: "100%", width: "100%" }}>
+                        <RigaGriglia inizio="1" fine="144" idLocation={idLocation} pxDimensioniMappa={pxDimensioniMappa} />
                     </div>
 
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="25" id="25"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="26" id="26"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="27" id="27"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="28" id="28"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="29" id="29"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="30" id="30"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="31" id="31"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="32" id="32"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="33" id="33"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="34" id="34"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="35" id="35"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="36" id="36"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="37" id="37"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="38" id="38"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="39" id="39"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="40" id="40"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="41" id="41"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="42" id="42"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="43" id="43"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="44" id="44"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="45" id="45"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="46" id="46"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="47" id="47"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="48" id="48"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="49" id="49"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="50" id="50"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="51" id="51"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="52" id="52"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="53" id="53"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="54" id="54"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="55" id="55"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="56" id="56"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="57" id="57"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="58" id="58"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="59" id="59"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="60" id="60"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="61" id="61"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="62" id="62"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="63" id="63"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="64" id="64"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="65" id="65"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="66" id="66"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="67" id="67"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="68" id="68"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="69" id="69"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="70" id="70"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="71" id="71"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="72" id="72"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="73" id="73"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="74" id="74"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="75" id="75"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="76" id="76"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="77" id="77"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="78" id="78"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="79" id="79"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="80" id="80"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="81" id="81"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="82" id="82"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="83" id="83"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="84" id="84"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="85" id="85"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="86" id="86"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="87" id="87"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="88" id="88"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="89" id="89"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="90" id="90"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="91" id="91"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="92" id="92"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="93" id="93"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="94" id="94"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="95" id="95"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="96" id="96"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="97" id="97"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="98" id="98"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="99" id="99"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="100" id="100"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="101" id="101"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="102" id="102"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="103" id="103"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="104" id="104"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="105" id="105"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="106" id="106"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="107" id="107"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="108" id="108"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="109" id="109"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="110" id="110"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="111" id="111"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="112" id="112"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="113" id="113"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="114" id="114"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="115" id="115"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="116" id="116"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="117" id="117"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="118" id="118"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="119" id="119"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="120" id="120"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="121" id="121"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="122" id="122"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="123" id="123"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="124" id="124"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="125" id="125"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="126" id="126"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="127" id="127"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="128" id="128"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="129" id="129"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="130" id="130"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="131" id="131"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="132" id="132"></div>
-                    </div>
-
-
-                    <div className="row" style={{ marginLeft: "0.01%" }}>
-                        <div className="col-sm-1 griglia-macromappa" title="133" id="133"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="134" id="134"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="135" id="135"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="136" id="136"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="137" id="137"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="138" id="138"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="139" id="139"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="140" id="140"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="141" id="141"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="142" id="142"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="143" id="143"></div>
-                        <div className="col-sm-1 griglia-macromappa" title="144" id="144"></div>
+                    <div style={{ position: "absolute", top: "0", zIndex: "3", height: "100%", width: "100%", display: `${lenteDisplay}` }}>
+                        {/* <GlassMagnifier imageSrc={macromappa} magnifierSize="80%" allowOverflow="false"/> */}
+                        <Magnifier imageSrc={macromappa} magnifierSize="80%" />
                     </div>
                 </div>
             </div>
-        );
-    }
+        </>
+    );
 }
 
 export default Macromappa;
