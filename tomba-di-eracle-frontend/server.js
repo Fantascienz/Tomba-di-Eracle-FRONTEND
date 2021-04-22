46b63075481cce88914b1c82317108f58328d3b3
Paolo Marchitto <paolo.marchitto88@gmail.com>
2021-04-22 17:26:37 +0200

    implementata parte della chat

--- tomba-di-eracle-frontend/server.js
+++ tomba-di-eracle-frontend/server.js
@@ -23,7 +23,7 @@ const io = socketio(server, {
 
 const urlDatabase = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.prkjr.mongodb.net/chat?retryWrites=true&w=majority';
 
-mongoose.connect(urlDatabase, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(urlDatabase, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
@@ -37,44 +37,59 @@ db.on('error', _ => {
 
 
 io.on('connect', (socket) => {
-    socket.on('join', ({name,room}, callback) => {
-      const { error, user } = addUser({ id: socket.id, name, room });
-      Msg.find({location:room}).then(result =>{
-        socket.emit('output-messages', result)
-        socket.emit('message', {  utente: 'admin', testo: `${user.name}, welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', { utente: 'admin', testo: `${user.name}, has joined!`});
-      })
-      if(error) return callback(error);
+  socket.on('join', ({ personaggio, location }, callback) => {
+    const { error, user } = addUser({ id: socket.id, personaggio, location });
+    Msg.find({ idLocation: location.id }).then(result => {
+      socket.emit('output-messages', result)
+      socket.emit('message', { testo: `${personaggio.nominativo},entra in ${location.nome}`, idLocation: location.id });
+      // socket.broadcast.to(user.location).emit('message', { utente: 'admin', testo: `${user.personaggio.nominativo}, has joined!` });
+    })
+    if (error) return callback(error);
+
+
+
+    socket.join(location);
+
+    // io.to(user.location).emit('roomData', { location: user.location, users: getUsersInRoom(user.location) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', ({ formValue, personaggio, location }, callback) => {
+    const user = getUser(socket.id);
+    const messaggioInviato = new Msg(
+      {
+        testo: formValue,
+        inviatoAlle: new Date(),
+        idPersonaggio: personaggio.id,
+        nomePersonaggio: personaggio.nominativo,
+        idLocation: location.id,
+        immagine: personaggio.urlImmagine
+      });
+    messaggioInviato.save().then(() => {
       
       
-      
-      socket.join(user.room);
-
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-
-      callback();
-    });
-
-    socket.on('sendMessage', (message, callback) => {
-      const user = getUser(socket.id);
-      const messaggio = new Msg({testo: message, utente: user.name, location: user.room});
-      messaggio.save().then(() => {
-        io.to(user.room).emit('message', { utente: user.name, testo: message });
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)})
-      })
-
-
-      callback();
+      socket.emit('message', {
+        testo: formValue,
+        nomePersonaggio: personaggio.nominativo,
+        idLocation: location.id,
+        immagine: personaggio.urlImmagine
+      });
+      // io.to(user.location).emit('roomData', { location: user.location, users: getUsersInRoom(user.location) })
     })
 
-    socket.on('disconnect', () => {
-      const user = removeUser(socket.id);
-      
-      if(user) {
-        io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left`}); 
-      }
-    
-    })
+
+    callback();
+  })
+
+  // socket.on('disconnect', () => {
+  //   const user = removeUser(socket.id);
+
+  //   if (user) {
+  //     io.to(location).emit('message', { user: 'admin', testo: `${user.personaggio.nominativo} has left` });
+  //   }
+
+  // })
 });
 
 app.use(router);
