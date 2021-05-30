46a30714c48c023f046c45b9532883af2f3a1885
Tiziano Massa <tiziano.max@gmail.com>
2021-05-30 12:02:45 +0200

    modificata gestione chiavi,adattata al nuovo backend

--- tomba-di-eracle-frontend/src/componenti/utils/ControllaChiave.js
+++ tomba-di-eracle-frontend/src/componenti/utils/ControllaChiave.js
@@ -1,12 +1,24 @@
+import axios from "axios"
 import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
 
 export const ControllaChiave = (chiave, location, metodoEntra) => {
-    if (chiave === location.chiave || location.chiave == null) {
-        metodoEntra(location.id)
+    if (location.hasChiave) {
+        axios.get("http://localhost:8080/chiavi/" + location.id + '/' + chiave).then(res => {
+            if (res.data) {
+                metodoEntra(location.id)
+            } else {
+                withReactContent(Swal).fire({
+                    title: <p>Chiave Errata!</p>
+                })
+            }
+        }).catch(err => {
+            withReactContent(Swal).fire({
+                title: <p>Inserisci una chiave!</p>
+            })
+        }
+        )
     } else {
-        withReactContent(Swal).fire({
-            title: <p>Chiave Errata!</p>
-        })
+        metodoEntra(location.id)
     }
 }
